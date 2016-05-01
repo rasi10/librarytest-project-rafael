ef50b8adb74236129c302bb6ac99ceeb644ae709
Rafael Silva <mind_traveller@hotmail.com>
2016-05-01 14:52:04 +0200

    First commit. I have just started the project

---
+++ src/test/java/se/nackademin/librarytest/rafael/LibraryApplicationTestSuite.java
@@ -0,0 +1,15 @@
+package se.nackademin.librarytest.rafael;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rafael
+ */
+public class LibraryApplicationTestSuite {
+    
+    public LibraryApplicationTestSuite() {
+    }
+    
+}
